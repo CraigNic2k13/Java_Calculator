b659a6e6b8723add15010c5953a2c7d70ee77d48
Craig Nichols <craignichols2k@hotmail.com>
2018-09-10 17:16:24 +0100

    Implemented basic math features, will do more later.

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,9 +1,28 @@
-
+/*******************************************************
+* Project : Java Calculator.                           *
+* Author  : Craig Nichols.                             *
+* Purpose : Calculator main functions.                 *
+*******************************************************/
 public class Main 
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
-	}
+		Basic_Math b = new Basic_Math(20, 8);
+		
+		System.out.println(b.Add());
+		System.out.println(b.Add(24, 80));
+		
+		System.out.println(b.Subtract());
+		System.out.println(b.Subtract(100, 50));
+		
+		System.out.println(b.Divide());
+		System.out.println(b.Divide(10, 5));
+		
+		System.out.println(b.Multiply());
+		System.out.println(b.Multiply(10, 30));
+		
+		System.out.println(b.Percentage());
+		System.out.println(b.Percentage(100, 5));
+	} 
 
 }
